eb192ff63b97965405d304f747d044ed8272be72
Philip Jägenstedt <philip@foolip.org>
2017-10-02 20:20:51 +0200

    Also report on specs that have no test dirs

--- update.node.js
+++ update.node.js
@@ -101,6 +101,45 @@ const TODO_WPT_DIRS = new Set([
   'x-frame-options',
 ])
 
+// spec ids for which no tests exist
+const TODO_SPEC_IDS = new Set([
+  'BackgroundSync',
+  'books',
+  'browserext',
+  'css-contain',
+  'css-content',
+  'css-device-adapt',
+  'css-inline',
+  'css-line-grid',
+  'css-overflow',
+  'css-page-floats',
+  'css-properties-values-api',
+  'css-scroll-snap',
+  'css-sizing',
+  'css-will-change',
+  'figures',
+  'fill-stroke',
+  'loader',
+  'longtasks',
+  'manifest',
+  'mediacapture-depth',
+  'mediacapture-screen-share',
+  'mimesniff',
+  'native-messaging',
+  'payment-method-manifest',
+  'permissions',
+  'push-api',
+  'requestidlecallback',
+  'resource-hints',
+  'svg-integration',
+  'svg-markers',
+  'svg-paths',
+  'svg-strokes',
+  'wake-lock',
+  'web-midi-api',
+  'webrtc-stats',
+])
+
 function getWptDirs() {
   const dir = `${REPO_CACHE_DIR}/github.com/w3c/web-platform-tests`
   const cmd = 'find * -maxdepth 0 -type d -print0; find css/* -maxdepth 0 -type d -print0'
@@ -143,6 +182,9 @@ function update() {
   const since = new Date(today - (common.NUM_DAYS + common.GRACE_DAYS) * DAY).toISOString()
   const until = new Date(today).toISOString()
 
+  // set of all dirs that (currently) really exist in wpt
+  const realWptDirs = getWptDirs()
+
   // set of all dirs in wpt that are used by some entry
   const usedWptDirs = new Set
 
@@ -154,10 +196,13 @@ function update() {
     fs.writeFileSync(`${DATA_DIR}/${entry.id}.test.log`, testLog)
 
     if (entry.testrepo.includes('web-platform-tests')) {
-      entry.testpath.split(' ').forEach(dir => {
-        // it doesn't matter that some of these directories don't exist
+      const entryDirs = entry.testpath.split(' ')
+      entryDirs.forEach(dir => {
         usedWptDirs.add(dir)
       })
+
+      if (!TODO_SPEC_IDS.has(entry.id) && !entryDirs.some(dir => realWptDirs.has(dir)))
+        console.info(`${entry.id} spec does not any wpt dirs (${entry.testpath} checked)`)
     }
   }
 
@@ -166,11 +211,11 @@ function update() {
   html = html.replace(/\d{4}-\d{2}-\d{2}/, new Date(today - DAY).toISOString().substr(0, 10))
   fs.writeFileSync('index.html', html)
 
+
   // report which wpt dirs don't have a corresponding entry
-  //console.log(usedWptDirs)
-  for (const dir of getWptDirs()) {
+  for (const dir of realWptDirs) {
     if (!usedWptDirs.has(dir))
-      console.warn(`${dir} is not claimed by any spec`)
+      console.warn(`${dir} dir is not claimed by any spec`)
   }
 }
 
