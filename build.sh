76cec6c6938f183aabfb1f0e8070be2c6db75b51
Philip Jägenstedt <philip@foolip.org>
2017-10-26 15:06:04 +0200

    Revert "Save the build and check logs to out/logs/"
    This reverts commit bc3b2cbf8c62047804ec07a2af89cea5c14566bd.
    
    Doing it in .travis.yml would delete the logs for non-Travis deploys.

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-mkdir -p out
+rm -rf out
+mkdir out
 node build/specs.js out/specs.json
 node build/data.js out/specs.json out/data.json
 cp static/* out/
