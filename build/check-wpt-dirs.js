59441bd1753f44090f1d61171cc91fad8ea010bd
Philip Jägenstedt <philip@foolip.org>
2017-10-26 14:32:02 +0200

    Extract out the wpt dir checks

---
+++ build/check-wpt-dirs.js
@@ -0,0 +1,97 @@
+'use strict'
+
+const execSync = require('child_process').execSync
+const fs = require('fs')
+const repo = require('./repo')
+
+const IGNORE_WPT_DIRS = new Set([
+  'acid', // Acid2 and Acid3
+  'annotation-model', // https://www.w3.org/TR/annotation-model/
+  'annotation-protocol', // https://www.w3.org/TR/annotation-protocol/
+  'annotation-vocab', // https://www.w3.org/TR/annotation-vocab/
+  'common',
+  'conformance-checkers',
+  'core-aam', // https://w3c.github.io/aria/core-aam/core-aam.html
+  'css', // tests are in subdirs
+  'css/CSS1', // https://www.w3.org/TR/CSS1/
+  'css/fonts',
+  'css/reference',
+  'css/support',
+  'css/tools',
+  'css/vendor-imports',
+  'css/work-in-progress',
+  'docs',
+  'dpub-aam', // https://w3c.github.io/aria/dpub-aam/dpub-aam.html
+  'dpub-aria', // http://w3c.github.io/aria/aria/dpub.html
+  'fonts',
+  'html-imports', // https://w3c.github.io/webcomponents/spec/imports/
+  'html-longdesc', // https://www.w3.org/TR/html-longdesc/
+  'images',
+  'infrastructure',
+  'interfaces',
+  'js', // https://github.com/w3c/web-platform-tests/issues/6462
+  'media',
+  'old-tests',
+  'resources',
+  'svg-aam', // https://w3c.github.io/aria/svg-aam/svg-aam.html
+  'tools',
+  'wai-aria', // http://w3c.github.io/aria/aria/aria.html
+  'wasm', // https://github.com/WebAssembly/spec/issues/529
+  'webgl', // https://github.com/w3c/web-platform-tests/issues/5927
+])
+
+function findWptDirs(dir) {
+  const cmd = 'find * -maxdepth 0 -type d -print0; find css/* -maxdepth 0 -type d -print0'
+  const dirs = execSync(cmd, { cwd: dir }).toString().split('\0').filter(dir => dir != '')
+  dirs.sort()
+  return new Set(dirs)
+}
+
+function main() {
+  const specsPath = process.argv[2]
+  console.assert(specsPath)
+
+  console.log(`Reading ${specsPath}`)
+  const specs = JSON.parse(fs.readFileSync(specsPath))
+
+  // set of all dirs that (currently) really exist in wpt
+  const wptDir = repo.checkout('https://github.com/w3c/web-platform-tests', { update: false })
+  const realWptDirs = findWptDirs(wptDir)
+
+  // set of all dirs in wpt that are used by some entry
+  const usedWptDirs = new Set
+
+  // list of specs (entries) for which no tests are found in wpt
+  const specsWithoutWptDirs = []
+
+  for (const entry of specs) {
+    // skips specs where the tests aren't int wpt
+    if (entry.testrepo && !entry.testrepo != 'w3c/web-platform-tests')
+      continue
+
+    // Note: some of the dirs may not exist, they are the arguments to git log
+    // TODO: this duplicates logic (entry.testpath || entry.id) in data.js
+    const dirs = (entry.testpath || entry.id).split(' ')
+
+    dirs.forEach(dir => usedWptDirs.add(dir))
+
+    if (!dirs.some(dir => realWptDirs.has(dir)))
+      specsWithoutWptDirs.push(entry)
+  }
+
+  if (specsWithoutWptDirs.length) {
+    console.log('Specs without tests (in wpt):')
+    for (const entry of specsWithoutWptDirs)
+      console.log(`  ${entry.name} <${entry.href}>`)
+  }
+
+  const wptDirsWithoutSpec = Array.from(realWptDirs)
+        .filter(dir => !usedWptDirs.has(dir) && !IGNORE_WPT_DIRS.has(dir))
+  if (wptDirsWithoutSpec.length) {
+    console.log('Directories (in wpt) without spec:')
+    for (const dir of wptDirsWithoutSpec)
+      console.log(`  ${dir}`)
+  }
+}
+
+main()
--- build/data.js
+++ build/data.js
@@ -7,123 +7,6 @@ const execSync = require('child_process').execSync
 const path = require('path')
 const repo = require('./repo')
 
-// dirs with no tests / old tests
-const IGNORE_WPT_DIRS = new Set([
-  'acid', // Acid2 and Acid3
-  'common',
-  'conformance-checkers',
-  'css', // tests are in subdirs
-  'css/CSS1', // https://www.w3.org/TR/CSS1/
-  'css/fonts',
-  'css/reference',
-  'css/support',
-  'css/tools',
-  'css/vendor-imports',
-  'css/work-in-progress',
-  'docs',
-  'fonts',
-  'images',
-  'infrastructure',
-  'interfaces',
-  'media',
-  'old-tests',
-  'resources',
-  'tools',
-])
-
-// dirs that need some kind of action
-const TODO_WPT_DIRS = new Set([
-  'annotation-model', // https://www.w3.org/TR/annotation-model/
-  'annotation-protocol', // https://www.w3.org/TR/annotation-protocol/
-  'annotation-vocab', // https://www.w3.org/TR/annotation-vocab/
-  'apng', // https://wiki.mozilla.org/APNG_Specification
-  'cookies', // https://github.com/w3c/web-platform-tests/pull/7531#issuecomment-333397939
-  'core-aam', // https://w3c.github.io/aria/core-aam/core-aam.html
-  'css-scroll-anchoring', // https://github.com/w3c/web-platform-tests/issues/7765
-  'css/WOFF2', // https://dev.w3.org/webfonts/WOFF2/spec/
-  'css/css-block-3', // https://github.com/w3c/web-platform-tests/issues/7652
-  'domxpath', // https://www.w3.org/TR/xpath/
-  'dpub-aam', // https://w3c.github.io/aria/dpub-aam/dpub-aam.html
-  'dpub-aria', // http://w3c.github.io/aria/aria/dpub.html
-  'editing',
-  'geolocation-API', // https://www.w3.org/TR/geolocation-API/
-  'html-imports', // https://w3c.github.io/webcomponents/spec/imports/
-  'html-longdesc', // https://www.w3.org/TR/html-longdesc/
-  'http', // https://tools.ietf.org/html/rfc7230
-  'js', // https://github.com/w3c/web-platform-tests/issues/6462
-  'mathml', // https://www.w3.org/TR/MathML/
-  'svg-aam', // https://w3c.github.io/aria/svg-aam/svg-aam.html
-  'trusted-types', // https://github.com/mikewest/trusted-types
-  'viewport', // https://github.com/w3c/web-platform-tests/issues/7749
-  'wai-aria', // http://w3c.github.io/aria/aria/aria.html
-  'wasm', // https://github.com/WebAssembly/spec/issues/529
-  'webgl', // https://github.com/w3c/web-platform-tests/issues/5927
-  'x-frame-options', // https://tools.ietf.org/html/rfc7034
-])
-
-// spec ids for which no tests exist
-const TODO_SPEC_IDS = new Set([
-  'BackgroundSync',
-  'InputDeviceCapabilities',
-  'animation-worklet',
-  'aom',
-  'books',
-  'browserext',
-  'budget-api',
-  'cors-rfc1918',
-  'css-contain',
-  'css-content',
-  'css-device-adapt',
-  'css-inline',
-  'css-line-grid',
-  'css-overflow',
-  'css-page-floats',
-  'css-properties-values-api',
-  'css-scroll-snap',
-  'css-sizing',
-  'css-will-change',
-  'dnt',
-  'execCommand',
-  'figures',
-  'fill-stroke',
-  'geolocation-sensor',
-  'manifest',
-  'mediacapture-depth',
-  'mediacapture-screen-share',
-  'mimesniff',
-  'mst-content-hint',
-  'native-messaging',
-  'payment-method-manifest',
-  'permissions',
-  'push-api',
-  'resource-hints',
-  'scroll-animations',
-  'scroll-boundary-behavior',
-  'shape-detection-api',
-  'svg-integration',
-  'svg-markers',
-  'svg-paths',
-  'svg-strokes',
-  'web-midi-api',
-  'webappsec-csp-embedded', // in content-security-policy/embedded-enforcement
-  'webrtc-stats',
-])
-
-function getWptDirs(dir) {
-  const cmd = 'find * -maxdepth 0 -type d -print0; find css/* -maxdepth 0 -type d -print0'
-  const dirs = execSync(cmd, { cwd: dir }).toString().split('\0').filter(dir => {
-    if (dir == '')
-      return false
-    if (IGNORE_WPT_DIRS.has(dir))
-      return false
-    if (TODO_WPT_DIRS.has(dir))
-      return false
-    return true
-  })
-  dirs.sort()
-  return new Set(dirs)
-}
-
 function getLog(dir, since, until, options) {
   // --date=short-local combined with TZ=UTC gets us the UTC date.
   let cmd = `git log --no-merges --since="${since}" --until="${until}" --date=short-local --pretty="%cd %h %s"`
@@ -180,16 +63,6 @@ function main() {
   const since = new Date(now - (config.days + 1) * DAY).toISOString()
   const until = new Date(now).toISOString()
 
-  // set of all dirs that (currently) really exist in wpt
-  const wptDir = repo.checkout('https://github.com/w3c/web-platform-tests')
-  const realWptDirs = getWptDirs(wptDir)
-
-  // set of all dirs in wpt that are used by some entry
-  const usedWptDirs = new Set
-
-  // list of specs (entries) for which no tests are found in wpt
-  const specsWithoutWptDirs = []
-
   console.log(`Reading ${specsPath}`)
   const specs = JSON.parse(fs.readFileSync(specsPath))
 
@@ -211,24 +84,11 @@ function main() {
     entry.speclog = specLog
     entry.testlog = testLog
 
-    if (TODO_SPEC_IDS.has(entry.id))
-      continue
-
     if (!entry.testpolicy) {
       // look for a testing policy (anywhere in the repo, ignore specPath)
       const testPolicy = getTestPolicy(specDir)
       if (testPolicy)
         entry.testpolicy = `https://github.com/${entry.specrepo}/blob/HEAD/${testPolicy}`
-    }
-
-    if (testRepo.includes('web-platform-tests')) {
-      const entryDirs = testPath.split(' ')
-      entryDirs.forEach(dir => {
-        usedWptDirs.add(dir)
-      })
-
-      if (!entryDirs.some(dir => realWptDirs.has(dir)))
-        specsWithoutWptDirs.push(entry)
     }
   }
 
@@ -247,22 +107,6 @@ function main() {
     specs: specs,
   }
   fs.writeFileSync(dataPath, JSON.stringify(data, null, '  ') + '\n')
-
-  // report on missing things
-
-  if (specsWithoutWptDirs.length) {
-    console.log('Specs without tests (in wpt):')
-    for (const entry of specsWithoutWptDirs)
-      console.log(`  ${entry.name} <${entry.href}>`)
-  }
-
-  const wptDirsWithoutSpec = Array.from(realWptDirs)
-        .filter(dir => !usedWptDirs.has(dir))
-  if (wptDirsWithoutSpec.length) {
-    console.log('Directories (in wpt) without spec:')
-    for (const dir of wptDirsWithoutSpec)
-      console.log(`  ${dir}`)
-  }
 }
 
 main()
