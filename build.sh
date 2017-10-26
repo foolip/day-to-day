bc3b2cbf8c62047804ec07a2af89cea5c14566bd
Philip Jägenstedt <philip@foolip.org>
2017-10-26 14:50:29 +0200

    Save the build and check logs to out/logs/
    Changes month-to-month might be interesting.

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-rm -rf out
-mkdir out
+mkdir -p out
 node build/specs.js out/specs.json
 node build/data.js out/specs.json out/data.json
 cp static/* out/
