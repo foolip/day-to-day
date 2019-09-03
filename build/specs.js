b3714b8f084580965915a731b18e0983d354de15
Philip Jägenstedt <philip@foolip.org>
2019-09-03 09:54:41 +0200

    Move `+` operators to appease eslint-config-google 0.14.0 (#26)

--- build/specs.js
+++ build/specs.js
@@ -225,8 +225,8 @@ function processRef(group, info) {
       specrepo: `w3c/${org}-drafts`,
       // Note: mediaqueries-5 has the highest level on 2017-09-30
       specpath: `${id} ${id}-1 ${id}-2 ${id}-3 ${id}-4 ${id}-5`,
-      testpath: `${id} css/${id} css/${id}-1 css/${id}-2 css/${id}-3 `
-          + `css/${id}-4 css/${id}-5`,
+      testpath: `${id} css/${id} css/${id}-1 css/${id}-2 css/${id}-3 ` +
+          `css/${id}-4 css/${id}-5`,
       testpolicy: testpolicy,
     };
   }
