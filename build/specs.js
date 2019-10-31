foolip/day-to-day

commit e2bad94a7d9d29b058c139e71392787173dc3f80

Account for new tc39.es URLs

build/specs.js
@@ -262,11 +262,31 @@ function processRef(group, info) {
     return entry;
   }
 
+  function entryFromTC39(url) {
+    console.assert(url.hostname == 'tc39.es');
+
+    const repoURL = new URL(info.repository);
+    console.assert(repoURL.hostname == 'github.com');
+
+    const [org, repo] = repoURL.pathname.split('/').filter(s => s);
+
+    return {
+      id: repo,
+      name: info.title,
+      href: url.href,
+      specrepo: `${org}/${repo}`,
+    };
+  }
+
   switch (group) {
     case 'biblio': {
       const url = new URL(info.href);
       if (url.hostname.endsWith('.github.io')) {
         return entryFromGitHubIO(url);
+      }
+
+      if (url.hostname == 'tc39.es') {
+        return entryFromTC39(url);
       }
 
       // TODO: handle more interesting things, like WebGL

commit a80dc1e23a204ad928fdcb24002a21e385bc027d

Fix lint
Broken by commit e2bad94a7d9d29b058c139e71392787173dc3f80.

build/specs.js
@@ -274,7 +274,7 @@ function processRef(group, info) {
     const repoURL = new URL(info.repository);
     console.assert(repoURL.hostname == 'github.com');
 
-    const [org, repo] = repoURL.pathname.split('/').filter(s => s);
+    const [org, repo] = repoURL.pathname.split('/').filter((s) => s);
 
     return {
       id: repo,